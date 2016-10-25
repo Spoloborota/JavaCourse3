871502851b1004dab0656861c0a6df8f34c7112a
rsayfutdinov <rsayfutdinov@cloudally.com>
2016-10-25 18:04:56 +0300

    strategy

---
+++ strategy/src/main/java/strategy/MiniDuckSimulator.java
@@ -0,0 +1,14 @@
+package strategy;
+
+import strategy.ducks.Duck;
+import strategy.ducks.MallardDuck;
+
+public class MiniDuckSimulator {
+ 
+	public static void main(String[] args) {
+ 
+		Duck mallard = new MallardDuck();
+		mallard.performQuack();
+		mallard.performFly();
+	}
+}
